dae1d1929950b8090531ee4940e476f03115b149
Krsna <monicamasson@gmail.com>
2022-05-01 18:03:59 -0400

    updated

--- server.js
+++ server.js
@@ -27,15 +27,66 @@ server.use(bodyParser.json())
 
 
     
-server.get("/routes/:greetingRouter", (req, res)=>{
+server.get("/routes/greeting", (req, res)=>{
     res.status(200).json({message: 'Hello stranger!'})
 })
 
-server.get("/routes/greetingRouter/.name.", (req, res)=>{
-    res.status(200).json({message: 'Hello' + req.params.greetingRouter})
+//server.get("/routes/greetingRouter/.name.", (req, res)=>{
+//    res.status(200).json({message: 'Hello' + req.params.greetingRouter})
+//})
+
+server.param('name', function(req, res, next, name) {
+   //const modified = name;
+  
+    req.name = name;
+    next();
+  });
+server.get('/routes/greeting/:name', function(req, res) {
+    res.send('Wow! Hello there ,' + req.name );
+  });
+
+// THis displays the tip questiom
+  server.get("/tip", (req, res)=>{
+    res.status(200).json({message: 'how much your tip will be'})
 })
 
+//Calculate tip
+server.get('/tip/:total/:percent', (req, res) => {
+    let Total = req.params.total;
+    let Percent = req.params.percent;
+    const Totaltip = (Total/100)*Percent;
+    res.send('Your tip would be ' + '$' + Totaltip );
+    })
 
+//Magic Ball answer
+server.get('/magic/:name', (req, res) => {
+    let answers = [ "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes - definitely.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful."];
+  let randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+  res.send( randomAnswer );
+   });
+
+// server.get('/routes/greeting/:name', function(req, res) {
+  //   res.send('Wow! Hello there ,' + req.name );
+   //});
 
 server.listen(PORT, ()=>{
     console.log(`Server is listening at: ${PORT}`)
